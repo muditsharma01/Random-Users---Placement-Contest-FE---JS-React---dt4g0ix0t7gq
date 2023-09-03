449fc2cda032dd531a384d7aceba70fd204b7eac
Mudit Sharma <muditshar001@gmail.com>
2023-09-03 18:28:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Users from "./users";
+import Users from "./Users";
 
 const App = () => {
-    const [items, setItems] = useState([]);
-    const [isLoading, setLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://randomuser.me/api/?results=10");
+        const data = response.data.results;
+        // Simulate a 3-second delay before setting loading to false
+        setTimeout(() => {
+          setItems(data);
+          setLoading(false);
+        }, 3000);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false); // Set loading to false in case of an error
+      }
+    };
 
-    return <Users isLoading={isLoading} items={items} />;
+    fetchData();
+  }, []);
+
+  return <Users isLoading={isLoading} items={items} />;
 };
 
 export default App;
